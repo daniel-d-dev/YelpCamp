61cb8aff50ff166c55aa74bfc40003943903175f
Daniel Davies <danieldaviesdevelopment@gmail.com>
2025-03-07 15:29:43 +0000

    Seeding random images from picsum

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
+const Campground = require('../models/campground');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
-const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -17,9 +17,13 @@ const seedDb = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur aliquam, a nisi quod, culpa adipisci atque maiores dicta veritatis excepturi cum, exercitationem recusandae provident perferendis vitae perspiciatis voluptas officiis.',
+            price
         })
         await camp.save();
     }
