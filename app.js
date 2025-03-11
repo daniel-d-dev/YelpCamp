e694564a282e537f7c10ab2a44de8294380f7e3d
Daniel Davies <danieldaviesdevelopment@gmail.com>
2025-03-11 16:48:21 +0000

    Created a basic error template to display error messages to the user

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -47,6 +48,7 @@ app.get('/campgrounds/new', (req, res) => {
 
 // Create Campground Route (POST) (Create)
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -79,9 +81,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }));
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found.', 404))
+});
+
 // Error Handler
 app.use((err, req, res, next) => {
-    res.send('Something went wrong...');
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh no, something went wrong!';
+    res.status(statusCode).render('error', { err });
 });
 
 // Server Start
