0303bc8daf3cf28bf4da636a34c510ef2766e7a1
Daniel Davies <danieldaviesdevelopment@gmail.com>
2025-04-14 20:29:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -45,10 +45,11 @@ const sessionConfig = {
     resave: false,
     saveUninitialized: true,
     cookie: {
+        httpOnly: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
+        maxAge: 1000 * 60 * 60 * 24 * 7,
     }
-}
+};
 
 app.use(session(sessionConfig));
 
