03ccdb4aba28bc1d94ab97818d22c68ec6c86cd1
Daniel Davies <danieldaviesdevelopment@gmail.com>
2025-04-14 21:19:19 +0100

    Created error partial and added error flash messages in appropriate places

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -50,12 +50,20 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 router.get('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id).populate('reviews');
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground });
 }));
 
 // Edit Campground Form Route (GET - Update Form)
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground })
 }));
 
